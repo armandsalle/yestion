a7a5f1711359e783c8defddbb239f49c640dacc0
Armand Sallé <armand.salle@gmail.com>
2021-03-24 08:33:11 +0100

    create the toto schema

--- pages/api/todo/[id].js
+++ pages/api/todo/[id].js
@@ -1,28 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import mongoose from "mongoose"
-import { Todo } from "../../lib/schemas/TodoSchema"
+import { Todo } from "../../../lib/schemas/TodoSchema"
 
 const handleGet = async (req, res) => {
   try {
-    const result = await Todo.find()
+    const { id } = req.query
+    const result = await Todo.findById(id)
     res.status(200).json({ todo: result })
   } catch (error) {
     console.error(error)
-    res.status(500).json({ status: "fail finding all the todos" })
-  }
-}
-
-const handlePost = async (req, res) => {
-  if (!req.body) {
-    res.status(500).json({ status: "fail. Need a todo" })
-  }
-
-  try {
-    const newTodo = await new Todo(req.body)
-    const result = await newTodo.save()
-    res.status(200).json({ todo: result })
-  } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json({ status: "fail fetching a todo" })
   }
 }
 
@@ -36,11 +23,9 @@ export default async (req, res) => {
       case "GET":
         await handleGet(req, res)
         break
-      case "POST":
-        await handlePost(req, res)
-        break
+
       default:
-        res.setHeader("Allow", ["GET", "POST"])
+        res.setHeader("Allow", ["GET"])
         res.status(405).json({
           status: 405,
           message: `Method ${method} Not Allowed`,
